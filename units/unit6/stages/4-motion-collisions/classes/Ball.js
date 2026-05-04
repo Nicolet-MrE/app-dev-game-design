94537abed3621121463f8dbb656de3c6075fb985
Neal Easterling <jimmy.easterling@nicolet.us>
2026-05-04 13:35:14 -0500

    refactor and update code to correct error

--- units/unit6/stages/4-motion-collisions/classes/Ball.js
+++ units/unit6/stages/4-motion-collisions/classes/Ball.js
@@ -1,38 +1,40 @@
 'use strict';
 
 export class Ball {
-    constructor(color, size, xPos, ){
-        this.id = String(Math.floor(Math.random() * 1000)); // New
-        this.x = Math.floor(Math.random() * (450 - 50) + 50); // New
-        this.y = 15;
-        this.yDir = 1; //New
-        this.xDir = this.getRandomDir(); //New
-        this.velocity = Math.random();
+    constructor(){
+        this.id = String(Math.floor(Math.random() * 1000));
         this.color = this.createRandomRGB();
-        this.size = Math.floor(Math.random() * (40 - 10) + 10); //new
+        this.size = Math.floor(Math.random() * (40-10) + 10);
+        this.x = Math.floor(Math.random() * (450-50) + 50);
+        this.y = 15;
+        this.yDir = 1;
+        this.xDir = this.getRandomDir();
+        this.velocity = Math.random();
         this.element = this.create();
         this.elementRef = undefined;
     }
 
     create(){
         const ball = document.createElement('div');
-        ball.id = this.id;  // new
-        ball.setAttribute('style', `width: ${this.size}px; height: ${this.size}px; background-color: ${this.color};`); //removed left from this list
-        ball.style.top = `${this.y}px`; // new
-        ball.style.left = `${this.x}px`; // new
+        ball.setAttribute('style', `width:${this.size}px; height:${this.size}px; background-color:${this.color};`);
+        ball.id = this.id;
+        ball.style.left = `${this.x}px`;
+        ball.style.top = `${this.y}px`;
         ball.classList.add('ball');
         console.log('ball created');
-        return ball;  
+        return ball;
     }
+
     getRandomDir(){
-        let test  = Math.random();
-        let dir = "1";
-        if(test >= 0.5) dir = "-1";
+        let test = Math.random();
+        let dir = 1;
+        if(test >=0.5 ) dir = -1;
         return dir;
     }
+
     createRandomRGB(){
         function random255(){
-            return Math.floor(Math.random() * 255); 
+            return Math.floor(Math.random() * 255);
         }
         let red = random255();
         let green = random255();
--- units/unit6/stages/4-motion-collisions/script.js
+++ units/unit6/stages/4-motion-collisions/script.js
@@ -1,36 +1,38 @@
-console.log('js connected!');
-import {Ball } from "./classes/Ball.js";
+import {Ball} from '/classes/Ball.js';
+console.log('js connected');
 
-window.onload = () => {
-     //most elements are new or refactored.
-    const container = document.getElementById('bounce-container');
-    const dropButton = document.getElementById('drop-button');
+const container = document.getElementById('bounce-container');
+const addButton = document.getElementById('drop-button');
+const balls = [];
 
-    const balls = [];
+addButton.addEventListener('click', ()=>{
+    let ball = new Ball();
+    container.appendChild(ball.element);
+    ball.elementRef = document.getElementById(ball.id);
+    balls.push(ball);
+});
 
-    dropButton.addEventListener('click', ()=>{
-        let ball = new Ball(); // adapted
-        container.appendChild(ball.element);
-        ball.elementRef = document.getElementById(ball.id); //New
-        balls.push(ball);
+function gameLoop(){
+    const yFactor = 8;
+    const xFactor = 5;
+
+    balls.forEach((ball)=>{
+        ball.yDir = (ball.y >= 300 - ball.size || ball.y <= 0) ? ball.yDir * -1 : ball.yDir;
+        ball.y = ball.y + ball.velocity * ball.yDir * yFactor;
+        ball.elementRef.style.top = `${ball.y}px`;
+        ball.xDir =(ball.x >= 500 - ball.size || ball.x <= 0) ? ball.xDir * -1 : ball.xDir;
+        ball.x = ball.x + ball.velocity * ball.xDir * xFactor;
+        ball.elementRef.style.left = `${ball.x}px`;
+        
+        
+        //console.log('tick');
+        
     });
 
-      function gameLoop(){
-        let yFactor = 5;
-        let xFactor = 2;
-        
-        balls.forEach((ball)=>{
-            ball.yDir = (ball.y >= 300 - ball.size/2 || ball.y <= -10 + ball.size /2) ? ball.yDir * -1 : ball.yDir;
-            ball.xDir = (ball.x >= 500 - ball.size/2 || ball.x <= 0 + ball.size /2) ? ball.xDir * -1 : ball.xDir;
-            ball.y = ball.y + ball.velocity * ball.yDir * yFactor;
-            ball.x = ball.x + ball.velocity * ball.xDir * xFactor;
-            ball.elementRef.style.top =  `${ball.y}px`;
-            ball.elementRef.style.left =  `${ball.x}px`;
-        });
-        requestAnimationFrame(gameLoop);
-    }
-
-    //Start Loop
     requestAnimationFrame(gameLoop);
-    
 }
+
+requestAnimationFrame(gameLoop);
+
+
+
